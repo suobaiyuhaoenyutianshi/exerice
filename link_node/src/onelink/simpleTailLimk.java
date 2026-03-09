e7494d0032183d6c281e8527a6b2566154fd8381
suobaiyuhaoenyutianshi <2467395920@qq.com>
2026-03-09 13:45:28 +0800

    delete的修改

--- link_node/src/onelink/simpleTailLimk.java
+++ link_node/src/onelink/simpleTailLimk.java
@@ -46,7 +46,37 @@ public class simpleTailLimk
     }
 
     //ok,再写个指定删除节点的
-    
+    public void simpledeleteTail(int x){
+        Node findObjct = this.head;
+        if(x== 0){
+            this.head = this.head.next;
+            findObjct.next = null;
+        }
+        // 辅助方法searchObiect,寻找对象的前个节点
+       /* while(x != 0){
+            findObjct = findObjct.next;
+            x--;
+        }*/
+        if(x != 0) {
+            Node findPrevObjct = searchObject(x);
+            //删除指定节点
+            findObjct = findPrevObjct.next;
+            findPrevObjct.next = findPrevObjct.next.next;
+            findObjct.next =null;
+        }
+        this.linkNum--;
+    }
+
+// 辅助方法searchObiect,寻找对象的前个节点
+    public Node searchObject(int x){
+        Node findObjct = this.head;
+        while(x != 1) {
+            findObjct = findObjct.next;
+            x--;
+        }
+        return  findObjct;
+    }
+
 //十分丑陋，想用递归，还不如直接计数
     private Node findNode(Node x){
 
